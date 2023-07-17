541b415ec0f2de7641d006ae02f0b4708773d36b
Ginza31 <lns-project@yandex.ru>
2023-07-17 18:17:57 +0300

    add changes from review

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,20 +7,6 @@
 
 import UIKit
 
-// MARK: Constants
-
-struct Constants {
-    static let firstColorValue = 0.05
-    static let secondColorValue = 0.27
-    static let thirdColorValue = 0.49
-    static let firstColorName = "Red:"
-    static let secondColorName = "Green:"
-    static let thirdColorName = "Blue:"
-    static let minimumValueForSlider = 0
-    static let maximumValueForSlider = 1
-    static let cornerRadius: CGFloat = 10
-}
-
 // MARK: ViewController
 
 class ViewController: UIViewController {
@@ -35,24 +21,38 @@ class ViewController: UIViewController {
     @IBOutlet var secondColorValue: UILabel!
     @IBOutlet var thirdColorValue: UILabel!
     
-    @IBOutlet var firstColorLabel: UILabel!
-    @IBOutlet var secondColorLabel: UILabel!
-    @IBOutlet var thirdColorLabel: UILabel!
-    
     // MARK: viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSlider()
-        setupUI()
+        
+        mixedColorView.layer.cornerRadius = 10
+        
+        setupSlider(firstColorSlider, color: .red, value: firstColorSlider.value)
+        setupSlider(secondColorSlider, color: .blue, value: secondColorSlider.value)
+        setupSlider(thirdColorSlider, color: .green, value: thirdColorSlider.value)
+        
+        setViewColor()
     }
     
     // MARK: Methods
 
-    @IBAction func sliderAction(_ sender: Any) {
-        firstColorValue.text = String(format: "%0.2f", firstColorSlider.value)
-        secondColorValue.text = String(format: "%0.2f", secondColorSlider.value)
-        thirdColorValue.text = String(format: "%0.2f", thirdColorSlider.value)
+    @IBAction func sliderAction() {
+        textColorForSlider(firstColorSlider, label: firstColorValue)
+        textColorForSlider(secondColorSlider, label: secondColorValue)
+        textColorForSlider(thirdColorSlider, label: thirdColorValue)
+        
+        setViewColor()
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupSlider(_ slider: UISlider, color: UIColor, value: Float) {
+        slider.value = value
+        slider.minimumTrackTintColor = color
+    }
+    
+    private func setViewColor() {
         mixedColorView.backgroundColor = UIColor(
             red: CGFloat(firstColorSlider.value),
             green: CGFloat(secondColorSlider.value),
@@ -61,40 +61,8 @@ class ViewController: UIViewController {
         )
     }
     
-    // MARK: - Private methods
-    
-    private func setupSlider() {
-        firstColorSlider.value = Float(Constants.firstColorValue)
-        firstColorSlider.minimumValue = Float(Constants.minimumValueForSlider)
-        firstColorSlider.maximumValue = Float(Constants.maximumValueForSlider)
-        firstColorSlider.minimumTrackTintColor = .red
-        
-        secondColorSlider.value = Float(Constants.secondColorValue)
-        secondColorSlider.minimumValue = Float(Constants.minimumValueForSlider)
-        secondColorSlider.maximumValue = Float(Constants.maximumValueForSlider)
-        secondColorSlider.minimumTrackTintColor = .green
-        
-        thirdColorSlider.value = Float(Constants.thirdColorValue)
-        thirdColorSlider.minimumValue = Float(Constants.minimumValueForSlider)
-        thirdColorSlider.maximumValue = Float(Constants.maximumValueForSlider)
-        thirdColorSlider.minimumTrackTintColor = .blue
+    private func textColorForSlider(_ slider: UISlider, label: UILabel) {
+        label.text = String(format: "%0.2f", slider.value)
     }
     
-    private func setupUI() {
-        firstColorLabel.text = Constants.firstColorName
-        secondColorLabel.text = Constants.secondColorName
-        thirdColorLabel.text = Constants.thirdColorName
-        
-        firstColorValue.text = String(Constants.firstColorValue)
-        secondColorValue.text = String(Constants.secondColorValue)
-        thirdColorValue.text = String(Constants.thirdColorValue)
-        
-        mixedColorView.layer.cornerRadius = Constants.cornerRadius
-        mixedColorView.backgroundColor = UIColor(
-            red: CGFloat(Constants.firstColorValue),
-            green: CGFloat(Constants.secondColorValue),
-            blue: CGFloat(Constants.thirdColorValue),
-            alpha: 1
-        )
-    }
 }
